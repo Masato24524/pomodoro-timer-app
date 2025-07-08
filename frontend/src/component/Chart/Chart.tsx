c2933e4ad97a46c16c7bbbde21fcc77627d2a5a5
Masato24524 <masato24524@gmail.com>
2025-07-08 16:15:51 +0900

    ✨ Chartコンポーネントに日付フォーマット関数を追加し、X軸を〇月〇日表記に。Y軸のドメインとティックを設定（1時間刻み、最大6時間（仮）とした）

--- frontend/src/component/Chart/Chart.tsx
+++ frontend/src/component/Chart/Chart.tsx
@@ -41,13 +41,24 @@ function Chart(rerenderTrigger:any) {
   //   );
   // };
 
+  // 日付フォーマット関数
+  const formatDate = (dateString: string):string => {
+    const date = new Date(dateString)
+    const month = date.getMonth() + 1;
+    const day = date.getDate()
+    return `${month}月${day}日`
+  }
+
   return (
     <div className="temp-container">
       {/* Rechartsで棒グラフを作成 */}
       <div className="chart-container">
         <BarChart width={400} height={200} data={data}>
-          <XAxis dataKey="name" tick={{ fontSize: 12 }} />
-          <YAxis />
+          <XAxis dataKey="name" tick={{ fontSize: 12 }} tickFormatter={formatDate}/>
+          <YAxis 
+            domain={[0, "dataMax"]}
+            ticks={[0,1,2,3,4,5,6]}
+            />
           <Bar
             dataKey="uv"
             barSize={30}
